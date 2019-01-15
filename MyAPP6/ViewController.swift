26411b5e5397c60184978aeb4347111358fde177
apple_mini <apple_mini@apple-minideMac-mini.local>
2019-01-15 13:51:15 +0800

    Initial Commit

---
+++ MyAPP6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAPP6
+//
+//  Created by apple_mini on 2019/1/15.
+//  Copyright © 2019年 ShangHaiBangBo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
